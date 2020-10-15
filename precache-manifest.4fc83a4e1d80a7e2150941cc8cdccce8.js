1e1ee55dbcd82958d7a01b5e144cc22af4b446f7
Arnab Sahoo <arnabsahoo10@gmail.com>
2020-10-16 04:54:01 +0530

    Updates

---
+++ precache-manifest.4fc83a4e1d80a7e2150941cc8cdccce8.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3d00ac89a4393e0013bb920cd31259",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "8b8af2ba5ea70b1c1d7f",
+    "url": "/my-portfolio/static/css/2.2a4cc010.chunk.css"
+  },
+  {
+    "revision": "75faed8951e968ec7589",
+    "url": "/my-portfolio/static/css/main.d01c64e9.chunk.css"
+  },
+  {
+    "revision": "8b8af2ba5ea70b1c1d7f",
+    "url": "/my-portfolio/static/js/2.e385b0e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.e385b0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75faed8951e968ec7589",
+    "url": "/my-portfolio/static/js/main.9c25108c.chunk.js"
+  },
+  {
+    "revision": "d7aefb2c0940c3e681b1",
+    "url": "/my-portfolio/static/js/runtime-main.7d4aa98d.js"
+  },
+  {
+    "revision": "4f88bce4181dd86d6557b01e48d7e196",
+    "url": "/my-portfolio/static/media/avtarimg.4f88bce4.svg"
+  },
+  {
+    "revision": "757b3959b3c545b6a135c9232f756474",
+    "url": "/my-portfolio/static/media/covid.757b3959.svg"
+  },
+  {
+    "revision": "85fb4ea5eee9d547890e92ba66c9fee2",
+    "url": "/my-portfolio/static/media/css3.85fb4ea5.svg"
+  },
+  {
+    "revision": "e5e924b63fd6befcdb17a030982f3ea5",
+    "url": "/my-portfolio/static/media/education.e5e924b6.svg"
+  },
+  {
+    "revision": "0fb30b49da79da6979871081a23bfd53",
+    "url": "/my-portfolio/static/media/expressjs.0fb30b49.svg"
+  },
+  {
+    "revision": "7c791d1b1e98ac59f06f7248bd7637a3",
+    "url": "/my-portfolio/static/media/github.7c791d1b.svg"
+  },
+  {
+    "revision": "9f2e1637c83e37e8f555044e12324734",
+    "url": "/my-portfolio/static/media/htmllogo.9f2e1637.svg"
+  },
+  {
+    "revision": "8ee02715beb101d7d505a9473bfb2754",
+    "url": "/my-portfolio/static/media/javascript.8ee02715.svg"
+  },
+  {
+    "revision": "3f69f636aec0737e25a13f643ad94636",
+    "url": "/my-portfolio/static/media/link.3f69f636.svg"
+  },
+  {
+    "revision": "2c20928ddc18c3e2ac70e6f8f38ca825",
+    "url": "/my-portfolio/static/media/linkedIn.2c20928d.svg"
+  },
+  {
+    "revision": "d571053a3cda81b67bfdaee97caab9bc",
+    "url": "/my-portfolio/static/media/mongodb.d571053a.svg"
+  },
+  {
+    "revision": "613cf57c00db5ba5c15b331620b6376f",
+    "url": "/my-portfolio/static/media/nodejslogo.613cf57c.svg"
+  },
+  {
+    "revision": "a7f9007647dcec4ee57ac9c8048c7d24",
+    "url": "/my-portfolio/static/media/project.a7f90076.svg"
+  },
+  {
+    "revision": "7038477259b6f1696308f867ce8c0a3a",
+    "url": "/my-portfolio/static/media/reactjs-icon.70384772.svg"
+  },
+  {
+    "revision": "0ec28acf205b307321676c2e8a320b0e",
+    "url": "/my-portfolio/static/media/skills.0ec28acf.svg"
+  },
+  {
+    "revision": "0afc2237b39a7715b12d0825b052f2f1",
+    "url": "/my-portfolio/static/media/street-food.0afc2237.svg"
+  },
+  {
+    "revision": "56d688644f33930069194d7c5fe3797e",
+    "url": "/my-portfolio/static/media/to-do-list.56d68864.svg"
+  },
+  {
+    "revision": "37caa6621fbf99d618c438b5a3c3ca5e",
+    "url": "/my-portfolio/static/media/twitter.37caa662.svg"
+  },
+  {
+    "revision": "d2dda26b5e4e91df27c6ae57ad094503",
+    "url": "/my-portfolio/static/media/weather.d2dda26b.svg"
+  }
+]);
