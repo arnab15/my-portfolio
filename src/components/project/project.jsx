6cf543c4b69da1bb6bdf5c294c1312b841e953ef
Arnab Sahoo <arnabsahoo10@gmail.com>
2021-07-02 23:45:30 +0530

    added projects

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -8,8 +8,40 @@ import weather from '../../asset/weather.svg';
 import streetFood from '../../asset/street-food.svg';
 import covid from '../../asset/covid.svg';
 import todoList from '../../asset/to-do-list.svg';
+import medical from '../../asset/medical-report.png';
+import healthCare from '../../asset/healthcare.png';
+import gallery from '../../asset/gallery.png';
 import { useEffect } from 'react';
 const PROJECT_DETAILS = [
+  
+  {
+    id: "A",
+    imgUrl: medical,
+    liveLink: 'https://covidmedicalresources.xyz/',
+    githubLink: 'https://github.com/arnab15/covid-medical-resources-backend',
+    title: 'Covid Medical Resources',
+    description:
+      'This application is build to find covid health resources during pandamic'
+  },
+  {
+    id: "B",
+    imgUrl: healthCare,
+    liveLink: 'https://uhms-eb710.web.app/',
+    githubLink: 'https://github.com/arnab15/UHMS-frontend',
+    title: 'UHMS',
+    description:
+      'A health monitoring system for students who are coming back to their institutions'
+  },
+  {
+    id: "A",
+    imgUrl: gallery,
+    liveLink: 'https://gallery-web.herokuapp.com/',
+    githubLink: 'https://github.com/arnab15/covid-medical-resources-backend',
+    title: 'GALLERY WEB',
+    description:
+      'It is a web gallery where user can upload their images and they can perform CRUD operatin on their images'
+  },
+  
   {
     id: 1,
     imgUrl: weather,
